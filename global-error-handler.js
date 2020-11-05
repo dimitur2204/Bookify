d682f68d41f4a26cbcd2acc076d855cb1cb0bfe6
Dimitar Nizamov <61479393+dimitur2204@users.noreply.github.com>
2020-11-05 09:27:21 +0200

    Added an error response class and handled CastErrors

--- global-error-handler.js
+++ global-error-handler.js
@@ -1,4 +1,15 @@
+const ErrorResponse = require("./utils/error-response");
+
 module.exports = (err,req,res,next) => {
+    let error = {...err};
+
     console.error(err);
-    res.status(500).end('Internal server error');
+    if(err.name === 'CastError'){
+        const message = `Item not found with id of ${err.value}`;
+        error = new ErrorResponse(message, 404);
+    }
+    res.status(error.statusCode || 500).json({
+        success:false,
+        error:error.message || 'Internal server error'
+    });
 }
