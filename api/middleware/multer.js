70f4f6f83f7923805b7b8fa5eaf85f7eecf7e5a7
Dimitar Nizamov <61479393+dimitur2204@users.noreply.github.com>
2020-11-06 10:08:29 +0200

    Added upload functionality for all the files

--- api/middleware/multer.js
+++ api/middleware/multer.js
@@ -1,6 +1,9 @@
 const multer = require('multer');
 const storage = multer.memoryStorage();
-const multerUploads = multer({ storage }).single('image');
+const multerUploads = multer({ storage }).fields([
+    {name:'image',maxCount:1}, 
+    {name:'preview',maxCount:1},
+    {name:'fullBook',maxCount:1}]);
 const Datauri = require('datauri/parser');
 const dUri = new Datauri();
 const path = require('path');
@@ -9,5 +12,5 @@ const path = require('path');
 * @param {Object} req containing the field object
 * @returns {String} The data url from the string buffer
 */
-const dataUri = req => dUri.format(path.extname(req.file.originalname).toString(), req.file.buffer);
+const dataUri = file => dUri.format(path.extname(file.originalname).toString(), file.buffer);
 module.exports = { multerUploads, dataUri };
