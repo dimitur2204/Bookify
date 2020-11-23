da631473a6f7ccf4353581c374d9bc3f0d9dab47
dimitur2204 <61479393+dimitur2204@users.noreply.github.com>
2020-11-23 17:51:29 +0200

    Not found page added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
+import { UserRoutingModule } from "./user/user-routing.module";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { CoreModule } from './core/core.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    UserRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
