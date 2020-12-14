ca573d50d0f12bda155c51cbc8a97181a36b67c3
Dimitar Nizamov <61479393+dimitur2204@users.noreply.github.com>
2020-12-14 23:49:20 +0200

    Fixed styles for the register component

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit,AfterViewInit {
 
+  @ViewChild('emailInput') emailInput:ElementRef|null;
   hidePass = true;
 
-  constructor() { }
+  constructor() {
+    this.emailInput = null;
+   }
+  ngAfterViewInit(): void {
+    this.emailInput?.nativeElement.focus();
+  }
 
   ngOnInit(): void {
   }
