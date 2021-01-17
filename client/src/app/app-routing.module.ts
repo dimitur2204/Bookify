ffcf1b2394cda104e1a377f76dd608983bec50e1
Dimitar Nizamov <61479393+dimitur2204@users.noreply.github.com>
2021-01-17 14:27:44 +0200

    Added navigation,create book component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
-import { BookListComponent } from './book/book-list/book-list.component';
+import { BookComponent } from './book/book/book.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
@@ -21,7 +21,7 @@ const routes: Routes = [
   },
   {
     path:'books',
-    component:BookListComponent
+    component:BookComponent
   }
 ];
 
